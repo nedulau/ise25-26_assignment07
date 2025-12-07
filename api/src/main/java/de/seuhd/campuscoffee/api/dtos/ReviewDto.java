47357ac715cc581b3d668e8ca342e5934c27236b
nedulau <27210700-nedulau@users.noreply.gitlab.com>
2025-12-07 23:08:58 +0100

    created review Dto and uncommented the review Dto mapper

--- api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
@@ -1,15 +1,33 @@
 package de.seuhd.campuscoffee.api.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import lombok.NonNull;
 import org.jspecify.annotations.Nullable;
+
+import java.time.LocalDateTime;
 
 /**
  * DTO record for POS metadata.
  */
 @Builder(toBuilder = true)
 public record ReviewDto (
-    @Nullable Long id
-    // TODO: Implement ReviewDto
+    @Nullable Long id,
+
+    @Nullable LocalDateTime createdAt, // is null when using DTO to create a new Review
+    @Nullable LocalDateTime updatedAt, // is set when creating or updating a Review
+
+    @NotNull
+    @NonNull Long posId,
+
+    @NotNull
+    @NonNull Long authorId,
+
+    @NotBlank(message = "Review cannot be empty.")
+    @NonNull String review,
+
+    @Nullable Boolean approved
 ) implements Dto<Long> {
     @Override
     public @Nullable Long getId() {
